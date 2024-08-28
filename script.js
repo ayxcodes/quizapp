b54d6829bfa996faef1bbe2d87933bf945625365
Aylin Celikhan <a.celikhan@outlook.de>
2024-08-28 12:47:08 +0200

    first question

---
+++ script.js
@@ -0,0 +1,74 @@
+let questions = [
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3
+    },
+    {
+        "question": "Was bedeutet das HTML Tag <a>?",
+        "answer_1": "Text Fett",
+        "answer_2": "Container",
+        "answer_3": "Ein Link",
+        "answer_4": "Kursiv",
+        "right_answer": 3
+    },
+    {
+        "question": "Wie bindet man eine Website in eine Website ein?",
+        "answer_1": "<iframe>, <frame> und <frameset>",
+        "answer_2": "<iframe>",
+        "answer_3": "<frame>",
+        "answer_4": "<frameset>",
+        "right_answer": 2
+    },
+    {
+        "question": "Wie stellt man Text am BESTEN fett dar?",
+        "answer_1": "<strong>",
+        "answer_2": "Css nutzen",
+        "answer_3": "<bold>",
+        "answer_4": "<b>",
+        "right_answer": 1
+    },
+    {
+        "question": "Welches Attribut kann man NICHT für Textarea verwenden?",
+        "answer_1": "readonly",
+        "answer_2": "max",
+        "answer_3": "from",
+        "answer_4": "spellcheck",
+        "right_answer": 1
+    },
+    {
+        "question": "Wie wählst du alle Elemente vom Typ <a> mit dem Attribut title aus?",
+        "answer_1": "a=title {...}",
+        "answer_2": "a > title {...}",
+        "answer_3": "a.title {...}",
+        "answer_4": "a[title]{...}",
+        "right_answer": 4
+    },
+    {
+        "question": "Wie definiert man in JavaScript eine Variable?",
+        "answer_1": "let 100 = rate;",
+        "answer_2": "let rate = 100;",
+        "answer_3": "100 = let rate;",
+        "answer_4": "rate = 100;",
+        "right_answer": 2
+    }
+];
+
+let currentQuestion = 0;
+
+function init() {
+    showQuestions();
+}
+
+function showQuestions() {
+    let question = questions[currentQuestion];
+
+    document.getElementById('questiontext').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
+}
