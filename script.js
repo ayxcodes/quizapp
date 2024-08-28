9fb55a1ac8895e8bb6dfc4b9c5aa0fcaa2b1c42e
Aylin Celikhan <a.celikhan@outlook.de>
2024-08-28 13:41:04 +0200

    answer function

--- script.js
+++ script.js
@@ -60,6 +60,8 @@ let questions = [
 let currentQuestion = 0;
 
 function init() {
+    document.getElementById('all-questions').innerHTML = questions.length;
+    
     showQuestions();
 }
 
@@ -71,4 +73,19 @@ function showQuestions() {
     document.getElementById('answer_2').innerHTML = question['answer_2'];
     document.getElementById('answer_3').innerHTML = question['answer_3'];
     document.getElementById('answer_4').innerHTML = question['answer_4'];
+}
+
+function answer(selection) {
+    let question = questions[currentQuestion];
+    console.log('Selected answer is', selection)
+    
+    let selectedQuestionNumber = selection.slice(-1);
+    console.log('selectedQuestionNumber is', selectedQuestionNumber);
+    console.log('Current question is', question['right_answer']);
+
+    if(selectedQuestionNumber == question['right_answer']) {
+        console.log('Richtige Antwort!');
+    } else {
+        console.log('Falsche Antwort!');
+    }
 }
